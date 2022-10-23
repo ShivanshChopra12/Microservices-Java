b3e1f0f8a438c5d75a7f38189f7043306037469e
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-10-24 02:08:32 +0530

    getmapping

--- trial-in-10-steps/src/main/java/com/basics/trialin10steps/BooksController.java
+++ trial-in-10-steps/src/main/java/com/basics/trialin10steps/BooksController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BooksController {
-	@GetMapping("/books")
-	public List<Book> getAllBooks()
-	{
-		return Arrays.asList(
-				new Book(1l, "Cloud Computing" , " Mr. Harvinder"));
+	@GetMapping("/shivansh") // whatever is in getmapping, that thing u have to mention with
+								// localhost:8080/shivansh
+	public List<Book> getAllBooks() {
+		return Arrays.asList(new Book(1l, "Cloud Computing", " Mr. Harvinder"));
 	}
 }
