d218ae6cb83beabd303cb24f547c08bcd389e2a3
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-03 00:47:55 +0530

    Update TrialIn10StepsApplication.java

--- trial-in-10-steps/src/main/java/com/basics/trialin10steps/TrialIn10StepsApplication.java
+++ trial-in-10-steps/src/main/java/com/basics/trialin10steps/TrialIn10StepsApplication.java
@@ -3,6 +3,7 @@ package com.basics.trialin10steps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TrialIn10StepsApplication {
 
