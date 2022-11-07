d50bac777230070c8848095e63f7c9cdd3631408
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-08 02:37:11 +0530

    Update Limits.java

--- limits-service/src/main/java/com/microservices/limitsservice/bean/Limits.java
+++ limits-service/src/main/java/com/microservices/limitsservice/bean/Limits.java
@@ -4,31 +4,33 @@ public class Limits {
 
 	private int minimum;
 	private int maximum;
-	
+
 	public int getMinimum() {
 		return minimum;
 	}
+
 	public void setMinimum(int minimum) {
 		this.minimum = minimum;
 	}
+
 	public int getMaximum() {
 		return maximum;
 	}
+
 	public void setMaximum(int maximum) {
 		this.maximum = maximum;
 	}
-	
-	//argumented constructor
+
+	// argumented constructor
 	public Limits(int minimum, int maximum) {
 		super();
 		this.minimum = minimum;
 		this.maximum = maximum;
 	}
-	//no argument constructor
+
+	// no argument constructor
 	public Limits() {
 		super();
 	}
-	
-	
-	
+
 }
