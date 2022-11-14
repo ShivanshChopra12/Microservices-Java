3cb86db87140dad0bd394e900130b06b8a127f9a
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-14 14:21:51 +0530

    loadbalancer

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -8,7 +8,14 @@ public class CurrencyExchange {
 	private String from;
 	private String to;
 	private BigDecimal conversionMultiple;
+	private String environment;
 	
+	public String getEnvironment() {
+		return environment;
+	}
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
 	public CurrencyExchange() {
 		
 	}
