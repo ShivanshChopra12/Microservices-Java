35cc993c1b768c4e0dce0753840c43fc71bd8ad8
ShivanshChopra12 <shivichopra.sc@gmail.com>
2022-11-29 00:49:14 +0530

    Update CurrencyExchangeController.java

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	//@Autowired
+	//private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
-		return new CurrencyExchange(1000L, from, to,
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,
 				BigDecimal.valueOf(50));
 		
+		//environment.
+		currencyExchange.setEnvironment("8080");
+		
+		return currencyExchange;
 	}
 }
  
